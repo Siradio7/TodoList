074a0fc6a30eb85bf2b7b08dff64254c4158916a
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-05-08 14:41:48 +0000

    Ajout de la fonctionnalité de modification des taches

--- tache.js
+++ tache.js
@@ -1,13 +1,40 @@
 let btnAjouter = document.getElementById("btn_ajouter")
 let listeTaches = document.getElementById("liste_taches")
+const taches = []
 let nbId = 0
 
-btnAjouter.addEventListener("click", () => {
+btnAjouter.addEventListener("click", (event) => {
+    if (event.target.innerText === "Modifier tache") {
+        let tache = document.getElementById("tache")
+        
+        const tache_courante = document.getElementById("tache_courante")
+        tache_courante.innerText = tache.value
+        const id_courant = document.getElementById("id_courant")
+
+        taches.splice(id_courant.value-1, 1, tache.value)
+
+        tache.value = ""
+        event.target.innerText = "Ajouter"
+        tache_courante.removeAttribute("id")
+        id_courant.remove()
+
+        return
+    }
+
     let tache = document.getElementById("tache")
 
     if (tache && tache.value.length > 0) {
+        const existed = taches.find(t => t === tache.value)
+
+        if (existed) {
+            tache.value = ""
+            return
+        }
+
         const tr = createElement(++nbId, tache.value)
         listeTaches.appendChild(tr)
+        taches.push(tache.value)
+        tache.value = ""
     }
 })
 
@@ -27,6 +54,8 @@ function createElement(tacheId, name) {
     btnSupprimer.innerText = "Supprimer"
 
     let btnModifier = document.createElement("button")
+    const btnId = "btn_modifier_".concat(tacheId)
+    btnModifier.setAttribute("id", btnId)
     btnModifier.classList.add("btn")
     btnModifier.classList.add("btn-sm")
     btnModifier.classList.add("btn-info")
